ac40f80d49c9189f4b1013691a96dfd403a27505
lelegendaire <112485248+lelegendaire@users.noreply.github.com>
2024-07-05 15:58:01 +0200

    Add files via upload

--- client.js
+++ client.js
@@ -1,331 +1,113 @@
-const socket = new WebSocket('wss://serveur-cq8x.onrender.com')
-console.log(socket)
-let isFirstTime = localStorage.getItem("isFirstTime");
-
-// Si isFirstTime est null (n'a pas encore été défini), définissez-le sur true
-if (isFirstTime === null) {
-    isFirstTime = true;
-    localStorage.setItem("isFirstTime", true);
-}
+const socket = new WebSocket('wss://serveur-cq8x.onrender.com');
 
 
+socket.addEventListener("open", () => {
+    if (!localStorage.getItem("firstTime_client")) {
+        console.log("C'est la première fois que l'utilisateur visite le site.");
+        localStorage.setItem("firstTime_client", "false");
+        socket.send(JSON.stringify({ action: "firstConnection", first: "true" }));
+    } else {
 
-// Événement déclenché lorsque la connexion WebSocket est ouverte
-socket.addEventListener("open", function (event) {
-    console.log("La connexion WebSocket est ouverte.");
-    console.log(isFirstTime)
-    // Vérifier si c'est la première fois
-    if (isFirstTime === true) {
-        const request2 = window.indexedDB.open("MaBaseDeDonnees", 1);
-        request2.addEventListener("success", function (event) {
-            const db = event.target.result;
-            const transaction = db.transaction("Updating", 'readwrite');
-            const objectStore = transaction.objectStore("Updating");
+        console.log("L'utilisateur a déjà visité le site auparavant 2.");
 
-            const currentTime = new Date();
-
-            // Obtention des composants de la date
-            const day = currentTime.getDate();
-            const month = currentTime.getMonth() + 1; // Les mois sont indexés à partir de 0
-            const year = currentTime.getFullYear();
-
-            // Formater la date au format "dd-mm-yyyy"
-            const formattedDate = `${day - 1 < 10 ? '0' : ''}${day - 1}-${month < 10 ? '0' : ''}${month}-${year}`;
-
-
-            // Obtention des composants de l'heure
-            const hours = currentTime.getHours();
-            const minutes = currentTime.getMinutes();
-
-            // Formater l'heure au format "hh:mm"
-            const formattedTime = `${(hours) < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
-
-            // Concaténer la date et l'heure dans la chaîne de caractères finale
-            const formattedDateTime = `${formattedDate} ${formattedTime}`;
-            objectStore.put({ MAJ: formattedDateTime, update: "maj_hour" })
-            console.log("C'est la première fois que la connexion WebSocket est ouverte. Ne rien faire.");
-            console.log(formattedDateTime);
-
-            isFirstTime = false; // Marquer que ce n'est plus la première fois
-            localStorage.setItem("isFirstTime", false); // Mettre à jour isFirstTime dans localStorage
-            return; // Sortir de la fonction
-        })
     }
+    console.log("WebSocket est ouvert.");
 
-    // Récupérer l'heure et la date stockées dans l'object store "Updating"
-    const request = window.indexedDB.open("MaBaseDeDonnees", 1);
-    request.addEventListener("success", function (event) {
-        const db = event.target.result;
-        const transaction = db.transaction("Updating", 'readonly');
-        const objectStore = transaction.objectStore("Updating");
-        const getRequest = objectStore.get("maj_hour"); // 1 étant l'identifiant unique dans l'object store "Updating"
-        console.log(getRequest)
-        getRequest.addEventListener('success', function (event) {
-            const serverDateTime = event.target.result.MAJ;
+});
 
-            // Comparer l'heure et la date du client avec celle du serveur
-            console.log("envoie donne date", serverDateTime)
+socket.addEventListener("message", (event) => {
+    console.log("Message reçu du serveur WebSocket:", event.data);
+    handleServerMessage(JSON.parse(event.data));
+});
 
-            socket.send(JSON.stringify({ action: "clientDateTime", dateTime: serverDateTime, first: "true" }));
-        });
-    });
+document.addEventListener('visibilitychange', () => {
+    if (document.visibilityState === 'visible') {
+
+    }
 });
 
 
-function toast_valid(text, event) {
-    const toastDiv = document.querySelector(".toast");
-    if (toastDiv) {
-        // Toast already exists
-        return;
-    } else {
-        // Créer la div principale avec la classe "toast"
-        const toastDiv = document.createElement("div");
-        toastDiv.classList.add("toast");
-        toastDiv.id = "toast";
+function requestInitialSync_FT() {
+    console.log("Demande de synchronisation initiale envoyée pour la première fois nouvelle connection.");
+    getDatabaseDateTime_TF((dateTime) => {
+        console.log(dateTime)
+        //socket.send(JSON.stringify({ action: "clientDateTime", dateTime: dateTime, first: "true" }));
+    });
+}
 
-        // Créer le contenu du toast
-        const toastContentDiv = document.createElement("div");
-        toastContentDiv.classList.add("toast-content");
 
-        const messageDiv = document.createElement("div");
-        messageDiv.classList.add("message");
-
-        const text2Span = document.createElement("span");
-        text2Span.classList.add("text", "text-2");
-        text2Span.textContent = text;
-
-        messageDiv.appendChild(text2Span);
-        toastContentDiv.appendChild(messageDiv);
-
-        if (event === "success") {
-            const checkIcon = document.createElement("i");
-            checkIcon.classList.add("bx", "bxs-check-circle");
-            checkIcon.style.color = "#0dff8ca8";
-            checkIcon.style.fontSize = "31px";
-            toastContentDiv.appendChild(checkIcon);
-        } else if (event === "error") {
-            const checkIcon = document.createElement("i");
-            checkIcon.classList.add("bx", "bxs-error-circle");
-            checkIcon.style.color = "#ff0d0da8";
-            checkIcon.style.fontSize = "31px";
-            toastContentDiv.appendChild(checkIcon);
-        }
-
-        // Ajouter les éléments au toastDiv
-        toastDiv.appendChild(toastContentDiv);
-
-        // Ajouter le toastDiv à un élément parent existant (par exemple, le body)
-        document.body.appendChild(toastDiv);
-
-        const style = `
-            .toast {
-                z-index: 999;
-                position: absolute;
-                top: 150px;
-                left: 0;
-                box-shadow: 0px 0px 10px rgb(0, 0, 0, 0.7), inset 0px 0px 10px rgb(0, 0, 0, 0.7);
-                background: #242020;
-                border: 2px solid rgb(66 66 66);
-                border-radius: 10px;
-                padding: 9px;
-                overflow: hidden;
-                transform: translateY(-250px) translateX(1050px);
-                transition: all 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.35);
+function handleServerMessage(message) {
+    if (message.action === 'latestDateTime') {
+        const latestDateTime = message.dateTime;
+        getDatabaseDateTime((localDateTime) => {
+            if (localDateTime === latestDateTime) {
+                sendDataToServer()
             }
-            .toast.active {
-                transform: translateY(0%) translateX(1050px);
-            }
-            .toast .toast-content {
-                display: flex;
-                align-items: center;
-            }
-            .toast-content .check {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                height: 35px;
-                width: 35px;
-                background-color: #4070f4;
-                color: #fff;
-                font-size: 20px;
-                border-radius: 50%;
-            }
-            .toast-content .message {
-                display: flex;
-                flex-direction: column;
-                margin: 0 12px;
-            }
-            .message .text {
-                font-size: 20px;
-                font-weight: 400;
-                color: #666666;
-            }
-            .message .text.text-1 {
-                font-weight: 600;
-                color: #333;
-            }
-        `;
+            console.log("Date local:" + localDateTime + "|" + "Date récente" + latestDateTime)
+        });
+    } else if (message.action === 'syncData') {
+        syncDataToDatabase(message.storeName, message.data);
+    } else if (message.action === "sendDate") {
 
-        // Créer une balise style
-        const styleElement = document.createElement("style");
-
-        // Ajouter le style au contenu
-        styleElement.appendChild(document.createTextNode(style));
-
-        // Ajouter le style au head du document
-        document.head.appendChild(styleElement);
-
-        setTimeout(() => {
-            toastDiv.classList.add("active");
-        }, 500); //1s = 1000 milliseconds
-        setTimeout(() => {
-            toastDiv.classList.remove("active");
-            setTimeout(() => {
-                document.body.removeChild(toastDiv);
-            }, 5000);
-        }, 5000); //1s = 1000 milliseconds
-
+        getDatabaseDateTime_TF((dateTime) => {
+            console.log("envoie de ma date:" + dateTime)
+            socket.send(JSON.stringify({ action: "clientDateTime", dateTime: dateTime, first: "true" }));
+        });
     }
 }
 
-// Événement déclenché lorsque le client reçoit un message du serveur WebSocket
-// Fonction pour traiter les données JSON
-function handleJSONData(jsonData) {
-    try {
-        console.log(jsonData)
-        const message = JSON.parse(jsonData);
-
-        // Vérifier l'action du message
-        if (message.action === 'update') {
-            // Mettre à jour la base de données côté client avec les données reçues
-            updateDatabaseWithData(message.data);
-        }
-    } catch (error) {
-        console.error("Erreur lors de l'analyse des données JSON:", error);
-    }
-}
-function handleBlobMessage(Blobdata) {
-    // Convertir les données Blob en chaîne
-    const blobReader = new FileReader();
-    blobReader.onload = function () {
-        const jsonData = blobReader.result;
-
-        // Traiter les données JSON
-        handleJSONData(jsonData);
+function getDatabaseDateTime_TF(callback) {
+    const request = indexedDB.open(dbName, dbVersion);
+    request.onsuccess = (event) => {
+        const db = event.target.result;
+        const transaction = db.transaction(["Updating"], 'readonly');
+        const store = transaction.objectStore("Updating");
+        const getRequest = store.get("maj_hour");
+        getRequest.onsuccess = () => {
+            callback(getRequest.result.MAJ);
+        };
     };
-    blobReader.readAsText(Blobdata);
 }
-// Fonction pour mettre à jour la base de données avec les données reçues
-
-function updateDatabaseWithData(data) {
-    const request = window.indexedDB.open("MaBaseDeDonnees", 1);
-    request.addEventListener("success", function (event) {
+function getDatabaseDateTime(callback) {
+    const request = indexedDB.open(dbName, dbVersion);
+    request.onsuccess = (event) => {
         const db = event.target.result;
-
-        data.forEach(function (item) {
-            const storeName = item.storeName;
-
-            const transaction = db.transaction(storeName, 'readwrite');
-            const objectStore = transaction.objectStore(storeName);
-
-            const request = objectStore.put(item.data);
-            request.addEventListener('success', function (event) {
-                console.log("Données mises à jour avec succès dans l'object store:", storeName);
-                // Afficher une notification de succès après la mise à jour des données
-                toast_valid("Mise à jour des données réussie", "success");
-            });
-            request.addEventListener('error', function (event) {
-                console.error("Erreur lors de la mise à jour des données dans l'object store:", storeName);
-                // Afficher une notification d'erreur en cas d'échec
-                toast_valid("Erreur lors de la mise à jour des données", "error");
-            });
-        });
-    });
+        const transaction = db.transaction(["Updating"], 'readonly');
+        const store = transaction.objectStore("Updating");
+        const getRequest = store.get("maj_hour");
+        getRequest.onsuccess = () => {
+            callback(getRequest.result.MAJ);
+        };
+    };
 }
 
-
-// Fonction pour traiter le message reçu du serveur WebSocket
-function handleMessageFromServer(event) {
-    console.log("Message reçu du serveur WebSocket:", event.data);
-    if (!(event.data instanceof Blob)) {
-        const message = JSON.parse(event.data);
-        if (message.action === 'latestDateTime') {
-
-
-            handleLatestDateTimeMessage(message);
-
-        } else {
-
-
-            handleJSONData(event.data);
-        }
-    } else if (event.data instanceof Blob) {
-
-        handleBlobMessage(event.data);
-    }
-}
-
-// Fonction pour traiter le message "latestDateTime" et comparer les dates
-function handleLatestDateTimeMessage(message) {
-    const latestDateTime = message.dateTime;
-    const request = window.indexedDB.open("MaBaseDeDonnees", 1);
-    request.addEventListener("success", function (event) {
+function syncDataToDatabase(storeName, data) {
+    const request = indexedDB.open(dbName, dbVersion);
+    request.onsuccess = (event) => {
         const db = event.target.result;
-        const transaction = db.transaction("Updating", 'readonly');
-        const objectStore = transaction.objectStore("Updating");
-        const getRequest = objectStore.get("maj_hour"); // 1 étant l'identifiant unique dans l'object store "Updating"
-        getRequest.addEventListener('success', function (event) {
-            const serverDateTime = event.target.result.MAJ;
-            console.log(serverDateTime)
-            console.log(latestDateTime)
-            if (serverDateTime >= latestDateTime) {
-                saveDataToServer();
-            } else {
-                console.log("Les données du serveur sont plus récentes. Aucune action requise.");
-            }
-        });
-    });
+        const transaction = db.transaction(storeName, 'readwrite');
+        const store = transaction.objectStore(storeName);
+
+        store.clear().onsuccess = () => {
+            data.forEach(item => store.put(item));
+            console.log(`Données synchronisées pour le store: ${storeName}`);
+        };
+    };
 }
 
-// Fonction pour sauvegarder les données sur le serveur
 
-function saveDataToServer() {
-    const request = window.indexedDB.open("MaBaseDeDonnees", 1);
-    request.addEventListener("success", function (event) {
+function sendDataToServer() {
+    const request = indexedDB.open(dbName, dbVersion);
+    request.onsuccess = (event) => {
         const db = event.target.result;
-        const objectStoreNames = db.objectStoreNames;
-        const allData = [];
-        const transaction = db.transaction(Array.from(objectStoreNames), 'readonly');
-        transaction.addEventListener('complete', function () {
-            if (allData.length > 0) {
-                socket.send(JSON.stringify({ action: "update", data: allData }));
-                // Afficher une notification de succès après l'envoi des données
-                toast_valid("Synchronisation terminée avec succès", "success");
-            } else {
-                console.log("Aucune donnée à envoyer au serveur.");
-            }
+        const stores = Array.from(db.objectStoreNames);
+        stores.forEach(storeName => {
+            const transaction = db.transaction(storeName, 'readonly');
+            const store = transaction.objectStore(storeName);
+            const getAllRequest = store.getAll();
+            getAllRequest.onsuccess = () => {
+                socket.send(JSON.stringify({ action: 'syncData', storeName: storeName, data: getAllRequest.result }));
+            };
         });
-        for (const storeName of objectStoreNames) {
-            if (storeName !== "Connexion") {
-                const objectStore = transaction.objectStore(storeName);
-                const cursorRequest = objectStore.openCursor();
-                cursorRequest.addEventListener('success', function (event) {
-                    const cursor = event.target.result;
-                    if (cursor) {
-                        allData.push({ storeName: storeName, data: cursor.value });
-                        cursor.continue();
-                    }
-                });
-            }
-        }
-    });
+    };
 }
-
-
-// Traitement des messages reçus du serveur WebSocket
-socket.addEventListener("message", handleMessageFromServer);
-
-// Initialisation de l'indicateur isFirstMessageReceived
-
-
-
--- database.js
+++ database.js
@@ -1,117 +1,113 @@
 
-// Fonction pour formater la date et l'heure
+const dbVersion = 1;
+let dbName = "MaBaseDeDonnees";
+function openDatabase() {
+    const request = indexedDB.open(dbName, dbVersion);
+    request.onupgradeneeded = (event) => {
+        const db = event.target.result;
+        createObjectStores(db);
+    };
+
+    request.onsuccess = (event) => {
+        const db = event.target.result;
+        monitorChanges(db);
+        if (!localStorage.getItem("firstTime")) {
+            console.log("C'est la première fois que l'utilisateur visite le site.");
+            localStorage.setItem("firstTime", "false");
+            updateDateTimeInUpdating();
+
+        } else {
+
+            console.log("L'utilisateur a déjà visité le site auparavant.");
+
+        }
+
+        setInterval(updateDateTimeInUpdating, 3600000); // Mise à jour toutes les heures
+    };
+}
+
+function createObjectStores(db) {
+    // Créer un objet de stockage (store) pour les FAQ
+    const ObjectStore = db.createObjectStore("Site", { keyPath: "id", autoIncrement: true });
+
+    // Ajouter un index pour rechercher par nom si nécessaire
+    ObjectStore.createIndex("SiteId", "id", { unique: false });
+    const ObjectStore_code = db.createObjectStore("Code", { keyPath: "id", autoIncrement: true });
+
+    // Ajouter un index pour rechercher par nom si nécessaire
+    ObjectStore_code.createIndex("CodeId", "id", { unique: false });
+    const ObjectStore2 = db.createObjectStore("Compte", { keyPath: "id", autoIncrement: true });
+    const ObjectStore3 = db.createObjectStore("Connexion", { keyPath: "connexion", autoIncrement: true });
+    const Updating = db.createObjectStore("Updating", { keyPath: "update", autoIncrement: true });
+    const preferencesStore = db.createObjectStore("preferences", {
+        keyPath: "name",
+        autoIncrement: true,
+    });
+    const Image_du_site = db.createObjectStore("Image_du_site", {
+        keyPath: "link",
+        autoIncrement: true,
+    });
+    const GestionSIte = db.createObjectStore("GestionSite", {
+        keyPath: "site_gestion",
+        autoIncrement: true,
+    });
+    // Ajouter un index pour rechercher par nom si nécessaire
+    ObjectStore2.createIndex("User", "username", { unique: false });
+    ObjectStore3.createIndex("Connected", "connected", { unique: false });
+    GestionSIte.createIndex("SiteGestion", "site", { unique: false });
+    ObjectStore3.createIndex("Name", "name_to_profil", { unique: false });
+    ObjectStore3.createIndex("userId", "id_to_create", { unique: false });
+    Image_du_site.createIndex("Image", "url", { unique: false });
+    Updating.createIndex("Updating", "MAJ", { unique: false });
+
+    // Ajoutez un index pour rechercher par nom de préférence
+    preferencesStore.createIndex("nameIndex", "name", { unique: true });
+    const formattedDateTime = getFormattedDateTime();
+    Updating.add({ MAJ: formattedDateTime, update: "maj_hour" });
+
+}
+
+function monitorChanges(db) {
+    const stores = Array.from(db.objectStoreNames); // Convertir en tableau
+    stores.forEach(storeName => {
+        const transaction = db.transaction(storeName, 'readonly');
+        const store = transaction.objectStore(storeName);
+        const request = store.openCursor();
+
+        request.onsuccess = (event) => {
+            const cursor = event.target.result;
+            if (cursor) {
+                cursor.continue();
+            }
+        };
+
+        transaction.oncomplete = () => {
+
+        };
+    });
+}
+
+function updateDateTimeInUpdating() {
+    const formattedDateTime = getFormattedDateTime();
+    const request = indexedDB.open(dbName, dbVersion);
+    request.onsuccess = (event) => {
+        const db = event.target.result;
+        const transaction = db.transaction(["Updating"], "readwrite");
+        const store = transaction.objectStore("Updating");
+        store.put({ MAJ: formattedDateTime, update: "maj_hour" });
+    };
+}
+
 function getFormattedDateTime() {
     const currentTime = new Date();
-
-    // Obtention des composants de la date
     const day = currentTime.getDate();
-    const month = currentTime.getMonth() + 1; // Les mois sont indexés à partir de 0
+    const month = currentTime.getMonth() + 1;
     const year = currentTime.getFullYear();
-
-    // Formater la date au format "dd-mm-yyyy"
     const formattedDate = `${day < 10 ? '0' : ''}${day}-${month < 10 ? '0' : ''}${month}-${year}`;
-
-    // Obtention des composants de l'heure
     const hours = currentTime.getHours();
     const minutes = currentTime.getMinutes();
-
-    // Formater l'heure au format "hh:mm"
     const formattedTime = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
-
-    // Concaténer la date et l'heure dans la chaîne de caractères finale
     return `${formattedDate} ${formattedTime}`;
 }
-// Fonction pour ouvrir la base de données
-function openDatabase() {
-    const request = indexedDB.open("MaBaseDeDonnees", 1);
-    const requestedBytes = 1024 * 1024 * 1000; // 10 Mo
-    let db;
-    navigator.storage.persist().then((granted) => {
-        if (granted) {
-            console.log("Quota accordé !");
-        } else {
-            console.error("Quota non accordé. L'utilisateur a refusé ou le quota maximal a été atteint.");
-        }
-    });
 
-    request.onupgradeneeded = function (event) {
-        // Cette méthode est appelée lorsqu'une nouvelle version de la base de données est créée
-        const db = event.target.result;
-
-        // Créer un objet de stockage (store) pour les FAQ
-        const ObjectStore = db.createObjectStore("Site", { keyPath: "id", autoIncrement: true });
-
-        // Ajouter un index pour rechercher par nom si nécessaire
-        ObjectStore.createIndex("SiteId", "id", { unique: false });
-        const ObjectStore_code = db.createObjectStore("Code", { keyPath: "id", autoIncrement: true });
-
-        // Ajouter un index pour rechercher par nom si nécessaire
-        ObjectStore_code.createIndex("CodeId", "id", { unique: false });
-        const ObjectStore2 = db.createObjectStore("Compte", { keyPath: "id", autoIncrement: true });
-        const ObjectStore3 = db.createObjectStore("Connexion", { keyPath: "connexion", autoIncrement: true });
-        const Updating = db.createObjectStore("Updating", { keyPath: "update", autoIncrement: true });
-        const preferencesStore = db.createObjectStore("preferences", {
-            keyPath: "name",
-            autoIncrement: true,
-        });
-        const Image_du_site = db.createObjectStore("Image_du_site", {
-            keyPath: "link",
-            autoIncrement: true,
-        });
-        const GestionSIte = db.createObjectStore("GestionSite", {
-            keyPath: "site_gestion",
-            autoIncrement: true,
-        });
-        // Ajouter un index pour rechercher par nom si nécessaire
-        ObjectStore2.createIndex("User", "username", { unique: false });
-        ObjectStore3.createIndex("Connected", "connected", { unique: false });
-        GestionSIte.createIndex("SiteGestion", "site", { unique: false });
-        ObjectStore3.createIndex("Name", "name_to_profil", { unique: false });
-        ObjectStore3.createIndex("userId", "id_to_create", { unique: false });
-        Image_du_site.createIndex("Image", "url", { unique: false });
-        Updating.createIndex("Updating", "MAJ", { unique: false });
-
-        // Ajoutez un index pour rechercher par nom de préférence
-        preferencesStore.createIndex("nameIndex", "name", { unique: true });
-
-        // Initialiser la date et l'heure dans Updating lors de la création de la base de données
-
-        // Concaténer la date et l'heure dans la chaîne de caractères finale
-        const formattedDateTime = getFormattedDateTime();
-
-        console.log(formattedDateTime); // Résultat : "24-03-2024 19:34"
-
-        Updating.add({ MAJ: formattedDateTime, update: "maj_hour" });
-    };
-
-    request.onsuccess = function (event) {
-        db = event.target.result;
-        // Mettre à jour la date et l'heure dans Updating à chaque modification dans la base de données
-
-        updateDateTimeInUpdating();
-
-        // Mettre à jour l'heure dans la base de données toutes les heures
-        setInterval(updateDateTimeInUpdating, 3600000); // 3600000 ms = 1 heure
-
-        db.onerror = function (event) {
-            console.error("Database error: " + event.target.errorCode);
-        };
-        // Mettre à jour la date et l'heure dans Updating chaque heure
-
-    };
-
-    function updateDateTimeInUpdating() {
-        const formattedDateTime = getFormattedDateTime();
-        // Mettre à jour l'heure dans Updating
-        const transaction = db.transaction(["Updating"], "readwrite");
-        const objectStore = transaction.objectStore("Updating");
-        objectStore.put({ MAJ: formattedDateTime, update: "maj_hour" });
-    }
-
-}
-
-
-// Exécuter la fonction pour ouvrir la base de données au chargement de la page
 openDatabase();
-
-
