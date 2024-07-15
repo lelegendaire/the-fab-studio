e51a402cc80902b5929724914b77f68969406310
lelegendaire <112485248+lelegendaire@users.noreply.github.com>
2024-07-15 21:15:58 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2059,10 +2059,7 @@ Newrequest.addEventListener("success", function (event) {
             };
 
         }
-    }
-
-}
-}
+    
 const plus_bento = document.querySelector('.plus_bento');
 const card_size_bento_plus = document.querySelector('.card_size_bento_plus');
 const wave_h = document.querySelector('.wave_h');
